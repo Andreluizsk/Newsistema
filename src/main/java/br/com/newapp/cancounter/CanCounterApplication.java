9e49262bd65a3ffa187bbfd3020d6c61f932daf4
Andre <Andreluizspfc@hotmail.com>
2022-06-27 14:23:42 -0300

    Primeiro Commit do Intellij IDEA

---
+++ src/main/java/br/com/newapp/cancounter/CanCounterApplication.java
@@ -0,0 +1,12 @@
+package br.com.newapp.cancounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanCounterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CanCounterApplication.class, args);
+
+    }
+}
---
+++ src/test/java/br/com/newapp/cancounterApplication/cancounter/cancounter/CancounterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.newapp.cancounterApplication.cancounter.cancounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
