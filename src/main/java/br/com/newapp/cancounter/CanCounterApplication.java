695599a6f657a6f48c3d36a0795658c3c863313d
Andreluizsk <andreluizspfc@hotmail.com>
2022-06-30 10:07:04 -0300

    Commit CanCounter Application

--- src/main/java/br/com/newapp/cancounter/CanCounterApplication.java
+++ src/main/java/br/com/newapp/cancounter/CanCounterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CanCounterApplication {
     public static void main(String[] args) {
         SpringApplication.run(CanCounterApplication.class, args);
-
+        System.out.println();
     }
 
 }
