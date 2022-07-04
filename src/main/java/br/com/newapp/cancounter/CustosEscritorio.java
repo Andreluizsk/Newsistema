c23c81182c04c6cdc24b90fae890b500b475d941
Andreluizsk <andreluizspfc@hotmail.com>
2022-07-04 10:07:16 -0300

    Adicionando o custo das Lampadas

--- src/main/java/br/com/newapp/cancounter/CustosEscritorio.java
+++ src/main/java/br/com/newapp/cancounter/CustosEscritorio.java
@@ -6,11 +6,13 @@ class CustosEscritorio {
     public static void main(String[] args) {
        int forro = 650;
         System.out.println("O valor do Forro foi " + forro );
-       int bancada = 400;
+       int bancada = 600;
         System.out.println("O valor da Bancada foi " + bancada);
-        int mesa = 200;
-        System.out.println("O valor da mesa foi " + mesa);
-        int total = forro + bancada + mesa;
+        int pintura = 400;
+        System.out.println("O valor da Pintura foi " + pintura);
+        int lampadas = 75;
+        System.out.println("O valor das Lampadas foi " + lampadas);
+        int total = forro + bancada + pintura + lampadas;
         System.out.println("O custo total foi de " + total);
     }
 
